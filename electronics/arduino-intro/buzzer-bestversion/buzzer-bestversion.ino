a41086e8dfb1011bb964856af506c9dca8e91cba
nolosaw <146073348+nolosaw@users.noreply.github.com>
2023-12-21 16:55:55 +0100

    update in the code

--- electronics/arduino-intro/buzzer-bestversion/buzzer-bestversion.ino
+++ electronics/arduino-intro/buzzer-bestversion/buzzer-bestversion.ino
@@ -1,15 +1,16 @@
 // pins des leds
-#define LEDR 2
-#define LEDV 3
-#define LEDB 4
-#define LEDJ 5
+#define LED0 A4
+#define LEDR A3
+#define LEDV A2
+#define LEDB A1
+#define LEDJ A0
 
 // pins des boutons
-#define BUTTONO 8
-#define BUTTONR 9
-#define BUTTONV 10
-#define BUTTONB 11
-#define BUTTONJ 12
+#define BUTTONO 1
+#define BUTTONR 2
+#define BUTTONV 3
+#define BUTTONB 4
+#define BUTTONJ 5
 
 // bits des leds dans la variable leds
 #define BIT_R 0
@@ -22,6 +23,7 @@ int leds = 0;
 
 void setup() {
   //INITIALISE LES LEDS
+  pinMode(LED0, OUTPUT);
   pinMode(LEDR, OUTPUT);
   pinMode(LEDV, OUTPUT);
   pinMode(LEDB, OUTPUT);
@@ -34,7 +36,8 @@ void setup() {
   pinMode(BUTTONB, INPUT_PULLUP);  //connect internal resistor to pin
   pinMode(BUTTONJ, INPUT_PULLUP);  //connect internal resistor to pin
 
-  //Turn the leds off when started
+  //Turn the leds off when started except the Reset button
+  digitalWrite(LEDR, HIGH
   digitalWrite(LEDR, LOW);
   digitalWrite(LEDV, LOW);
   digitalWrite(LEDB, LOW);
@@ -102,22 +105,21 @@ void setLeds(int leds) {
 }
 
 void acceptSequence() {
-    setLeds(0b1000);
-    delay(100);
-    setLeds(0b0100);
-    delay(100);
-    setLeds(0b0010);
-    delay(100);
-    setLeds(0b0001);
-    delay(100);
+  setLeds(0b1000);
+  delay(100);
+  setLeds(0b0100);
+  delay(100);
+  setLeds(0b0010);
+  delay(100);
+  setLeds(0b0001);
+  delay(100);
 
-    setLeds(0b0010);
-    delay(100);
-    setLeds(0b0100);
-    delay(100);
-    setLeds(0b1000);
-    delay(100);
-    setLeds(0b0000);
-    delay(100);
-
+  setLeds(0b0010);
+  delay(100);
+  setLeds(0b0100);
+  delay(100);
+  setLeds(0b1000);
+  delay(100);
+  setLeds(0b0000);
+  delay(100);
 }
